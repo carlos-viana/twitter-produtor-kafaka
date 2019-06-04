22e1ed070209aa055b5fb59b03979848e304835d
Treinamento Tecgraf <treinamento@lab003.tecgraf.puc-rio.br>
2019-06-04 10:59:31 -0300

    Atualizando o objeto de modelo Tweet para poder ficar consistente com o consumior.

--- src/main/java/Tweet.java
+++ src/main/java/Tweet.java
@@ -1,17 +1,30 @@
 import java.util.Date;
+import java.util.Objects;
 
 public class Tweet {
 	private String username;
 	private String text;
-	private Date date;
+	private Date creationDate;
 
-	public Tweet(String username, String text, Date date) {
+	public Tweet(String username, String text, Date creationDate) {
 		this.username = username;
 		this.text = text;
-		this.date = date;
+		this.creationDate = creationDate;
 	}
 
-	public String getUsername() {
+	public void setUserName(String username) {
+		this.username = username;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getUserName() {
 		return username;
 	}
 
@@ -19,12 +32,31 @@ public class Tweet {
 		return text;
 	}
 
-	public Date getDate() {
-		return date;
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Tweet that = (Tweet) o;
+		return Objects.equals(username, that.username) &&
+				Objects.equals(text, that.text) &&
+				Objects.equals(creationDate, that.creationDate);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(username, text, creationDate);
 	}
 
 	@Override
 	public String toString() {
-		return "Username: " + this.username + " \n " + this.text + " Date:\n" + this.date.toString();
+		return "Tweet: " +
+				"\nData criação: " + creationDate +
+				"\nNome do usuario:' " + username +
+				"\nMensagem: \n" + text + "\n";
 	}
+
 }
